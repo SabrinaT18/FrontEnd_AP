87b4a453e82ebf0bc1c2eedbbed19aa73d4fe84d
SabrinaT18 <96924905+SabrinaT18@users.noreply.github.com>
2022-11-18 21:30:37 -0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login/login.component';
 import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { GuardGuard } from './guard.guard';
 
 const routes: Routes = [
-    { path: 'portfolio',/*  canActivate:[GuardGuard], */  component: PortfolioComponent},
+    { path: 'portfolio', canActivate:[GuardGuard], component: PortfolioComponent},
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: 'login' , pathMatch: 'full' },
   ];
